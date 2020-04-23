bcee584cb9bc2ecd8313208929120d2b7f7e3d9b
nadome-19 <64146938+nadome-19@users.noreply.github.com>
2020-04-23 17:03:46 +0200

    everything

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 let name = 'Adam'
 
-console.log(name + "'s apples"
-);
+console.log(name + "'s apples");
 console.log('Hello World');
+
+$('h1').text('We are having a break until 17:05')
+
 // This is java script comment
+
